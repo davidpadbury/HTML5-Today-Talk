f5ed67c5cffb5fa7af3f4738f7a16d9215205a30
David Padbury <david@davidpadbury.com>
2010-11-08 16:37:04 -0500

    graphics demo (basically just everything with the grid)

---
+++ demos/demo6/index.js
@@ -0,0 +1,185 @@
+
+require.paths.unshift(__dirname + '/src');
+
+var express = require('express'),
+	app = express.createServer(),
+	io = require('socket.io'),
+	createSymbol = require('symbol').create,
+	assetManager = require('connect-assetmanager'),
+	socket;
+
+
+var clients = {},
+	tickers = {};
+	
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyDecoder());
+	app.use(express.methodOverride());
+	app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+	app.use(app.router);
+	app.use(assetManager({
+		'app-js': {
+			route: /\/js\/Marketr.js/,
+			path: './public/js/Marketr/',
+			dataType: 'javascript',
+			debug: true,
+			files: [
+				'*'
+			]
+		}
+	}));
+	app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+app.get('/order', function(req, res) {
+	var firstSymbol = (function() {
+		for (var p in tickers) {
+			return p;
+		}
+		return null;
+	})();
+	res.render('order', {
+		locals: {
+			errz: [],
+			user: '',
+			symbol: firstSymbol,
+			size: '100'
+		}
+	});
+});
+
+app.post('/order', function(req, res) {
+	var rules = [
+		{ name: 'user', valid: function(v) { return v; }, message: 'Big up yerself! (You need a username)' },
+		{ name: 'symbol', valid: function(v) { return tickers[v]; }, message: 'Use a symbol uz seen'},
+		{ name: 'size', valid: function(v) { return parseInt(v, 10) > 0; }, message: 'BUYZ MORE PLZ (size > 0)' }
+	], errz = [];
+	
+	rules.forEach(function(rule) {
+		if ( !rule.valid( req.body[rule.name] ) ) {
+			errz.push( rule.message );
+		}
+	});
+	
+	if (errz && errz.length) {
+		res.render('order', {
+			locals: {
+				errz: errz,
+				user: req.body.user,
+				symbol: req.body.symbol,
+				size: req.body.size
+			}
+		});
+	} else {
+		makeOrder( req.body.symbol, req.body.user, parseInt( req.body.size ));
+		res.render('ordered');
+	}
+});
+
+app.listen(3006);
+
+function tick(symbol, price) {
+	var clientIds = tickers[symbol] ? tickers[symbol].clients : [],
+		i, l;
+		
+	for (i = 0, l = clientIds.length; i < l; i++) {
+		clients[clientIds[i]].client.send(JSON.stringify({
+			topic: 'tick',
+			symbol: symbol,
+			price: price
+		}));
+	}
+}
+
+function createTicker(s) {
+	var symbol = createSymbol(s);
+	symbol.on('tick', tick);
+	return {
+		symbol: symbol,
+		orders: [],
+		clients: []
+	};
+}
+
+function makeOrder(symbol, user, size) {
+	var ticker = tickers[symbol],
+		clientIds = ticker.clients || [],
+		order = {
+			user: user,
+			size: size
+		},
+		i,l;
+		
+	ticker.orders.push(order);
+	
+	for (i = 0, l = clientIds.length; i < l; i++) {
+		clients[clientIds[i]].client.send(JSON.stringify({
+			topic: 'new-order',
+			symbol: symbol,
+			user: user,
+			size: size
+		}));
+	}
+}
+
+socket = io.listen(app);
+
+socket.on('connection', function(client) {
+	var id = client.sessionId;
+	
+	clients[id] = {
+		client: client,
+		tickers: []
+	};
+	
+	function getTicker(symbol) {
+		return tickers[symbol] || (tickers[symbol] = createTicker(symbol));
+	}
+	
+	client.on('message', function(s) {
+		var data = JSON.parse(s);
+		
+		switch (data.topic) {
+			case 'subscribe-tick':
+				var ticker = getTicker(data.symbol);
+				
+				ticker.clients.push(client.sessionId);
+				client.send(JSON.stringify({
+					topic: 'tick',
+					symbol: data.symbol,
+					price: ticker.symbol.currentPrice()
+				}));
+				break;
+				
+			case 'unsubscribe-tick':
+				var ticker = tickers[data.symbol];
+				for (var i = 0; i < ticker.clients.length; i++) {
+					if (ticker.clients[i] === client.sessionId) {
+						ticker.clients.splice(i, 1);
+						break;
+					}
+				}
+				break;
+				
+			case 'request-recent-data':
+				var ticker = getTicker(data.symbol);
+				client.send(JSON.stringify({
+					topic: 'recent-data',
+					symbol: data.symbol,
+					prices: ticker.symbol.getRecentPrices(),
+					orders: ticker.orders.slice( -20 )
+				}));
+		}
+	});
+	
+});
---
+++ demos/demo6/public/js/Marketr/Marketr.SymbolListViewModel.js
@@ -0,0 +1,29 @@
+(function(Marketr) {
+
+	Marketr.SymbolsListViewModel = function() {
+		this.lookupText = ko.observable('');
+		this.items = ko.observableArray([]);
+	};
+
+	Marketr.SymbolsListViewModel.prototype = {
+		lookupSymbol: function(e) {
+			var symbol = this.lookupText();
+		
+			this.addSymbol(symbol);
+			this.lookupText('');
+		},
+	
+		addSymbol: function(symbol) {
+			symbol = symbol.toUpperCase();
+		
+			var itemViewModel = new Marketr.SymbolListItemViewModel( symbol );
+			$(itemViewModel).bind('remove', $.proxy(this.removeSymbol, this));
+			this.items.push( itemViewModel );
+		},
+	
+		removeSymbol: function(e, s) {
+			this.items.remove( s );
+		}
+	};
+
+})(window.Marketr || (window.Marketr = {}));
