79ea5596ec13f731f31fc0b8298740bd31009bef
David Padbury <david@davidpadbury.com>
2010-11-08 15:50:30 -0500

    removed stuff from socket.io demo

--- demos/demo4/index.js
+++ demos/demo4/index.js
@@ -23,60 +23,11 @@ app.configure(function(){
 	app.use(express.staticProvider(__dirname + '/public'));
 });
 
-function tick(symbol, price) {
-	var clientIds = tickers[symbol] ? tickers[symbol].clients : [],
-		i, l;
-		
-	for (i = 0, l = clientIds.length; i < l; i++) {
-		clients[clientIds[i]].client.send(JSON.stringify({
-			topic: 'tick',
-			symbol: symbol,
-			price: price
-		}));
-	}
-}
-
-function createTicker(s) {
-	var symbol = createSymbol(s);
-	symbol.on('tick', tick);
-	return {
-		symbol: symbol,
-		clients: []
-	};
-}
-
-function getTicker(symbol) {
-	return tickers[symbol] || (tickers[symbol] = createTicker(symbol));
-}
-
 app.listen(3004);
 socket = io.listen(app);
 console.log( 'Listening on 3004' );
 
 
-var clients = {},
-	tickers = {};
-
-
 socket.on('connection', function(client) {
-	var id = client.sessionId;
-	
-	clients[id] = {
-		client: client,
-		tickers: []
-	};
-	
-	client.on('message', function(s) {
-		var data = JSON.parse(s);
-		
-		var ticker = getTicker(data.symbol);
-		
-		ticker.clients.push(client.sessionId);
-		client.send(JSON.stringify({
-			topic: 'tick',
-			symbol: data.symbol,
-			price: ticker.symbol.currentPrice()
-		}));
-	});
 	
 });
